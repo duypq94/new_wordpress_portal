f779fc563c99efd6a9d4f02a2cbcf71a3dbb3e78
nguyentiennghiep <nguyen_tien_nghiep@mediado.jp>
2019-01-21 14:58:32 +0900

    Add fb plugin

--- header.php
+++ header.php
@@ -8,6 +8,15 @@
     <?php wp_head(); ?>
  </head>
  <body>
+ 	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2';
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
+
     <div class="container-fluid logo">
         <div class="container">
             <img src="/wp-content/themes/new_portal/asset/imgs/logo_bkwh-1.jpg" alt="Logo">
@@ -15,24 +24,5 @@
     </div>
 
     <div class="container primary-menu">
-       <!--  <ul class="menu">
-            <li>Top</li>
-            <li>ビジョン</li>
-            <li>ナレッジ</li>
-            <li>グループ</li>
-            <li>ライブラリ</li>
-            <li>各種申請</li>
-            <li>各種リンク</li>
-            <li class="drop-menu">お問合せ
-                <ul class="sub-menu">
-                    <li>総務部へ問い合わせ</li>
-                    <li>管理へ問い合わせ</li>
-                    <li>人事部べ問い合わせ</li>
-                    <li>インフラ部へ問い合わせ</li>
-                    <li>情シスへ問い合わ</li>
-                    <li>どこへ問い合わせすればわからない場合コチラ</li>
-                </ul>
-            </li>
-        </ul> -->
         <?php portal_menu('primary-menu') ?>
     </div>
