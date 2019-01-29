bb3a711e059cda96e3e564fca01f547e6520433a
vuxuanlam <lam.vuxuan2512@gmail.com>
2019-01-29 11:15:02 +0900

    add page.php and content.php

--- page.php
+++ page.php
@@ -1,1 +1,14 @@
-<h1>Page</h1>
+<?php get_header(); ?>
+<div class="container">
+	<h1><?php echo get_the_title();  ?></h1>
+    <?php if( have_posts() ) : while( have_posts()) : the_post(); ?>
+        <?php get_template_part('content',get_post_format()); ?>
+    <?php endwhile; ?>
+    <?php else: ?>
+        <?php get_template_part('content','none'); ?>
+    <?php endif; ?>
+      
+</div>
+  
+
+<?php get_footer(); ?>
