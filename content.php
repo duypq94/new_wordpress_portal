0872356dbeabb850d91d90ab9710e8f201714a02
nguyentiennghiep <nguyen_tien_nghiep@mediado.jp>
2019-01-29 10:59:30 +0900

    add otoiawase

---
+++ content.php
@@ -0,0 +1,7 @@
+<div id = "post-<?php the_ID() ?>" class = "post-detail"<?php post_class(); ?> >
+	<div class="entry-header">
+	</div>
+	<div class="entry-content">
+		<?php the_content(); ?>
+	</div>
+</div>
--- custom-templates/otoiawase.php
+++ custom-templates/otoiawase.php
@@ -2,12 +2,19 @@
 	/* 
 	Template Name:お問い合わせ
 	*/
-?>
-<?php get_header();?>
-<div class="container">
-<div id="application-form">
-    <h2>お問い合わせ</h2>
-    <?php echo do_shortcode('[contact-form-7 id="82" title="お問い合わせ"]'); ?>
-    </div>
-</div>
-<?php get_footer();?>
+	?>
+	<?php get_header();?>
+	<div class="container-fluid main-content">
+		<div class="container main-page" id="otoiawase-page">
+			<div class="card">
+				<h3><?php echo get_the_title(); ?></h3>
+				<?php if( have_posts() ) : while( have_posts()) : the_post(); ?>
+					<?php get_template_part('content',get_post_format()); ?>
+				<?php endwhile; ?>
+				<?php else: ?>
+					<?php get_template_part('content','none'); ?>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
+	<?php get_footer();?>
