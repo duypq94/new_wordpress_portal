c5a5f6c849cd95065a7bd632a595a4ee1b401e06
nguyentiennghiep <nguyen_tien_nghiep@mediado.jp>
2019-01-31 14:21:35 +0900

    appli

--- custom-templates/application.php
+++ custom-templates/application.php
@@ -5,9 +5,67 @@ Template Name: Application
 ?>
 <?php get_header();?>
 <div class="container-fluid main-content">
-    <div class="container main-page">
+    <div class="container main-page application-page">
 		<div class="card">
-			<?php echo do_shortcode('[MEC id="177"]'); ?>
+			<div class="kanren ">
+				<h3>関連で探す</h3>
+				<div class="kanren-icons">
+					<div class="kanren-icon">
+						<a href="#"><img src="<?php echo get_template_directory_uri()?>/asset/imgs/links_img/work.png"" alt="">
+							<p>メール関連</p></a>
+					</div>
+					<div class="kanren-icon">
+						<a href="#"><img src="<?php echo get_template_directory_uri()?>/asset/imgs/links_img/work.png"" alt="">
+							<p>メール関連</p></a>
+					</div>
+					<div class="kanren-icon">
+						<a href="#"><img src="<?php echo get_template_directory_uri()?>/asset/imgs/links_img/work.png"" alt="">
+							<p>メール関連</p></a>
+					</div>
+					<div class="kanren-icon">
+						<a href="#"><img src="<?php echo get_template_directory_uri()?>/asset/imgs/links_img/work.png"" alt="">
+							<p>メール関連</p></a>
+					</div>
+					<div class="kanren-icon">
+						<a href="#"><img src="<?php echo get_template_directory_uri()?>/asset/imgs/links_img/work.png"" alt="">
+							<p>メール関連</p></a>
+					</div>
+					<div class="kanren-icon">
+						<a href="#"><img src="<?php echo get_template_directory_uri()?>/asset/imgs/links_img/work.png"" alt="">
+							<p>メール関連</p></a>
+					</div>
+					<div class="kanren-icon">
+						<a href="#"><img src="<?php echo get_template_directory_uri()?>/asset/imgs/links_img/work.png"" alt="">
+							<p>メール関連</p></a>
+					</div>
+					<div class="kanren-icon">
+						<a href="#"><img src="<?php echo get_template_directory_uri()?>/asset/imgs/links_img/work.png"" alt="">
+							<p>メール関連</p></a>
+					</div>
+				</div>
+			</div>
+			<div class="pata-n kanren">
+				<h3>パターンで探す</h3>
+				<div class="kanren-icons">
+					<div class="kanren-icon">
+						<a href="#"><img src="<?php echo get_template_directory_uri()?>/asset/imgs/links_img/work.png"" alt="">
+							<p>メール関連</p></a>
+					</div>
+					<div class="kanren-icon">
+						<a href="#"><img src="<?php echo get_template_directory_uri()?>/asset/imgs/links_img/work.png"" alt="">
+							<p>メール関連</p></a>
+					</div>
+					<div class="kanren-icon">
+						<a href="#"><img src="<?php echo get_template_directory_uri()?>/asset/imgs/links_img/work.png"" alt="">
+							<p>メール関連</p></a>
+					</div>
+					<div class="kanren-icon">
+						<a href="#"><img src="<?php echo get_template_directory_uri()?>/asset/imgs/links_img/work.png"" alt="">
+							<p>メール関連</p></a>
+					</div>
+				</div>
+			</div>
+			</div>
 		</div>
 	</div>
 </div>
