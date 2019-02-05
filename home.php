461be78e706c6fbc22bf169cba683cdf3c04f045
nguyentiennghiep <nguyen_tien_nghiep@mediado.jp>
2019-02-05 16:20:23 +0900

    add tabjs

--- home.php
+++ home.php
@@ -1,26 +1,22 @@
 <?php get_header(); ?>
-
 <div class="container-fluid main-content">
     <div class="container main-page">
         <div class="left-column">
-            <!-- <div class="calendar card"> -->
-            <!-- <div class="card-title">
-                    <i class="fas fa-calendar-alt"></i>イベントカレンダー
-                    
-                </div>
-                <?php echo do_shortcode( '[MEC id="117"]' );?>
-            </div> -->
-
-            <div class="noti tab-card card">
+            <div class="card card-js">
                 <div class="card-title">
-                    <i class="fas fa-clipboard"></i>各部署のお知らせ
+                    <i class="fas fa-newspaper"></i>ニュース
                 </div>
-                <ul class="tabs">
-                    <li>
-                        <input type="radio" name="tab" id="id-1" checked>
-                        <label for="id-1">最新</label>
-                        <div class="tabBody">
-                            <?php 
+                <div class="card-content">
+                    <div class="tab">
+                        <button class="tablinks active" onclick="openTab(event, 'id101')" id="defaultOpen">最近</button>
+                        <button class="tablinks" onclick="openTab(event, 'id102')">総務部</button>
+                        <button class="tablinks" onclick="openTab(event, 'id103')">経理部</button>
+                        <button class="tablinks active" onclick="openTab(event, 'id104')" id="defaultOpen">人事部</button>
+                        <button class="tablinks active" onclick="openTab(event, 'id105')" id="defaultOpen">技術部</button>
+                    </div>
+
+                    <div id="id101" class="tabcontent">
+                          <?php 
                             $the_query = new WP_Query( array( 'post_type'=> array('soumu','keiri','zinzi','gizyutsu'), 
                                 'posts_per_page' => '5',
                                 'orderby'=>'date',
@@ -34,15 +30,15 @@
                                 <a href="<?php the_permalink(); ?>" class="link-info-title">
                                     <?php the_title(); ?><span class="link-info-label">
                                         <?php
-                                                
-                                                $post_type = get_post_type();
-                                                if ( $post_type )
-                                                {
-                                                    $pt = get_post_type_object( $post_type  );
-                                                    echo $pt->labels->singular_name;
-                                                }
 
-                                                ?>
+                                                    $post_type = get_post_type();
+                                                    if ( $post_type )
+                                                    {
+                                                        $pt = get_post_type_object( $post_type  );
+                                                        echo $pt->labels->singular_name;
+                                                    }
+
+                                                    ?>
                                     </span></a>
                                 <p class="link-info-short-details">
                                     <?php echo utf8_truncate(get_the_excerpt());  ?>
@@ -55,37 +51,21 @@
                                 <?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?>
                             </p>
                             <?php endif; ?>
-                        </div>
-                    </li>
-                    <li>
-                        <input type="radio" name="tab" id="id-2">
-                        <label for="id-2">総務部</label>
-                        <div class="tabBody">
-                            <?php load_post_by_post_type('soumu','5'); ?>
-                        </div>
-                    </li>
-                    <li>
-                        <input type="radio" name="tab" id="id-3">
-                        <label for="id-3">経理部</label>
-                        <div class="tabBody">
-                            <?php load_post_by_post_type('keiri','5'); ?>
-                        </div>
-                    </li>
-                    <li>
-                        <input type="radio" name="tab" id="id-4">
-                        <label for="id-4">人事部</label>
-                        <div class="tabBody">
-                            <?php load_post_by_post_type('zinzi','5'); ?>
-                        </div>
-                    </li>
-                    <li>
-                        <input type="radio" name="tab" id="id-5" value="gijutsubu">
-                        <label for="id-5">技術部</label>
-                        <div class="tabBody">
-                            <?php load_post_by_post_type('gizyutsu','5'); ?>
-                        </div>
-                    </li>
-                </ul>
+                    </div>
+
+                    <div id="id102" class="tabcontent">
+                        <?php load_post_by_post_type('soumu','5'); ?>
+                    </div>
+
+                    <div id="id103" class="tabcontent">
+                        <?php load_post_by_post_type('keiri','5'); ?>
+                    </div>
+                    <div id="id104" class="tabcontent">
+                         <?php load_post_by_post_type('zinzi','5'); ?>
+                    </div>
+                    <div id="id105" class="tabcontent">
+                         <?php load_post_by_post_type('gizyutsu','5'); ?>
+                    </div>
             </div>
 
             <div class="system tab-card card">
@@ -153,6 +133,7 @@
                     </li>
                 </ul>
             </div>
+
             <div class="news tab-card card">
                 <div class="card-title">
                     <i class="fas fa-newspaper"></i>ニュース
@@ -214,76 +195,11 @@
                     </li>
                 </ul>
             </div>
-            <div class="news123 card">
-                <div class="card-title">
-                    <i class="fas fa-newspaper"></i>ニュース
-                </div>
-                <div class="naiyo">
-                    <div class="tab">
-                        <button class="tablinks active" onclick="openCity(event, 'id101')" id="defaultOpen">最近</button>
-                        <button class="tablinks" onclick="openCity(event, 'id102')">総務部</button>
-                        <button class="tablinks" onclick="openCity(event, 'id103')">経理部</button>
-                        <button class="tablinks active" onclick="openCity(event, 'id104')" id="defaultOpen">人事部</button>
-                        <button class="tablinks active" onclick="openCity(event, 'id105')" id="defaultOpen">技術部</button>
-                    </div>
 
-                    <div id="id101" class="tabcontent">
-                        <?php 
-                            $the_query = new WP_Query( array( 'post_type'=> array('soumu','keiri','zinzi','gizyutsu'), 
-                                'posts_per_page' => '5',
-                                'orderby'=>'date',
-                                'ignore_sticky_posts' => 1,) ); ?>
-                            <?php if ( $the_query->have_posts() ) : ?>
-                            <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
-                            <div class="link-info">
-                                <p class="publish-date">
-                                    <?php echo get_the_date(); ?>
-                                </p>
-                                <a href="<?php the_permalink(); ?>" class="link-info-title">
-                                    <?php the_title(); ?><span class="link-info-label">
-                                        <?php
-                                                
-                                                $post_type = get_post_type();
-                                                if ( $post_type )
-                                                {
-                                                    $pt = get_post_type_object( $post_type  );
-                                                    echo $pt->labels->singular_name;
-                                                }
-
-                                                ?>
-                                    </span></a>
-                                <p class="link-info-short-details">
-                                    <?php echo utf8_truncate(get_the_excerpt());  ?>
-                                </p>
-                            </div>
-                            <?php endwhile; ?>
-                            <?php wp_reset_postdata(); ?>
-                            <?php else : ?>
-                            <p>
-                                <?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?>
-                            </p>
-                            <?php endif; ?>
-                    </div>
-
-                    <div id="id102" class="tabcontent">
-                        <?php load_post_by_post_type('soumu','5'); ?>
-                    </div>
-
-                    <div id="id103" class="tabcontent">
-                       <?php load_post_by_post_type('keiri','5'); ?>
-                    </div>
-                    <div id="id104" class="tabcontent">
-                         <?php load_post_by_post_type('zinzi','5'); ?>
-                    </div>
-                    <div id="id105" class="tabcontent">
-                        <?php load_post_by_post_type('gizyutsu','5'); ?>
-                    </div>
-                </div>
-            </div>
         </div>
         <div class="right-column">
             <?php get_template_part('/includes/side-bar/right-sidebar') ?>
         </div>
     </div>
-
-    <?php get_footer(); ?>
+</div>
+<?php get_footer(); ?>
