51e2ea8e1ed4ae1178fac56a0930594cad01e231
nguyentiennghiep <nguyen_tien_nghiep@mediado.jp>
2019-02-19 10:49:52 +0900

    fix footer

--- footer.php
+++ footer.php
@@ -1,9 +1,16 @@
 
 <div class="container-fluid footer">
 	<div class="container">
-		<?php 
-		printf('<a href="%1$s"><img src="%2$s/asset/imgs/logo_bkwh-1.jpg" alt="media-logo" class="md-logo"></a>',get_bloginfo('url'), get_template_directory_uri());
-		?> 
+		<div style="font-size: 18px; font-weight: 600; margin-top: 40px">株式会社メディアドゥホールディングス </div>
+	</br>
+	<div style="margin-bottom: 20px;">
+		<p style="font-size: 16px; font-weight: 600;margin-bottom: 10px;">ひとつでも多くのコンテンツを、ひとりでも多くの人へ</p>
+		<p style="font-size: 14px; color: #ccc;">メディアドゥグループは、「ひとつでも多くのコンテンツを、ひとりでも多くの人に届ける」ため、 デジタルコンテンツのビジネスノウハウや、最先端テクノロジーによって構築されたソリューション／プラットフォームを 活用した「電子書籍流通事業」「メディア・プロモーション事業」を推進し、著作物のデジタル流通をグローバルに展開していきます。</p>
+	</div>
+
+	<div style="margin-bottom: 20px;">
+		<p style="font-size: 10px; color: #f1f1f1;">Copyright <i class="fa fa-copyright" style="margin-right: 0px;"></i> 2019 MEDIA DO HOLDINGS Co., Ltd.</p>
+	</div>
 	</div>
 </div>
 <?php wp_footer(); ?>
