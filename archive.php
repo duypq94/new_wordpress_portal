f9d2bce1db8235c83954910a76fe952f96763604
duypq2394 <duypq.jp@gmail.com>
2019-03-12 17:48:33 +0900

    test add calendar

---
+++ archive.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+<div class="container-fluid main-content">
+<div class="container main-page archive-page">
+	<div class="card">
+		<h1 style="font-size: 30px;"><?php echo get_the_date(); ?><span style="font-style: italic;"><?php echo (get_query_var( 's' )) ?></span> </h1>
+		 <?php if( have_posts() ) : while( have_posts()) : the_post(); ?>		
+		   <div class="search-result">
+                <a href="<?php the_permalink(); ?>" >
+                <h3><?php the_title(); ?></h3></a>         
+					<p class="publish-information">
+                    <?php 	printf( __('<span class = "tag-date-time"><i class="far fa-clock"></i>   %1$s </span>', 'portal'),get_the_date() );
+							printf( __('<span class = "tag-author"><i class="fas fa-user"></i>   %1$s </span>', 'portal'),get_the_author() ); ?>
+                	</p>
+                    <p class="expert-information"><?php echo utf8_truncate(get_the_excerpt());  ?></p>
+
+            </div>
+		 <?php endwhile; ?>
+		 <?php else: ?>
+	     <?php get_template_part('content','none'); ?>
+		 <?php endif; ?>
+		 <?php portal_pagination(); ?>
+	</div>
+
+</div>
+
+<?php get_footer(); ?> 
+     
