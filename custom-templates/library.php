a744fab349663f6fabdf412fb618d5fbd388280e
vuxuanlam <lam.vuxuan2512@gmail.com>
2019-03-13 11:26:06 +0900

    fix library page

--- custom-templates/library.php
+++ custom-templates/library.php
@@ -1,8 +1,8 @@
-<?php 
-	/* 
-	Template Name: Library
-	*/
-    ?>
+<?php
+/*
+Template Name: Library
+ */
+?>
     <?php get_header();?>
     <div class="container-fluid main-content">
         <div class="container main-page" id="libary-page-id">
@@ -19,30 +19,29 @@
                 </div>
             <div id="LibraryPageId101" class="tabcontentLibraryPage tabcontent">
                 <?php echo do_shortcode('[wpdm_all_packages jstable=1 order_by="title" order="DESC"  cols="title,|categories|update_date|download_link" colheads="タイトル|カテゴリー|最終更新日付|ダウンロード／閲覧"]'); ?>
-            </div>  
-            
+            </div>
+
             <div id="LibraryPageId102" class="tabcontentLibraryPage tabcontent">
                 <div>
                     <h5 class="ul-title">法務部</h5>
-                    <h4>..........作成中..........</h4>  
+                    <h4>..........作成中..........</h4>
                 </div>
-            </div>  
-
+                <?php get_template_part('/includes/custom-template/library/houmu-tab') ?>
             <div id="LibraryPageId103" class="tabcontentLibraryPage tabcontent">
                 <div>
                     <h5 class="ul-title">社内規程（MD）</h5>
-                    <h4>..........作成中..........</h4>  
+                    <h4>..........作成中..........</h4>
                 </div>
-            </div>                       
+            </div>
 
             <div id="LibraryPageId104" class="tabcontentLibraryPage tabcontent">
                 <div>
                     <h5 class="ul-title">社内規程（MD）</h5>
-                    <h4>..........作成中..........</h4>  
+                    <h4>..........作成中..........</h4>
                 </div>
      </div>
  </div>
-</div> 
+</div>
 </div>
 </div>
 <?php get_footer();?>
