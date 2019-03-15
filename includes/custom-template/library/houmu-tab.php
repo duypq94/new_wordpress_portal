6c043c73ce139e70914538836d8bb1b9e502d199
vuxuanlam <lam.vuxuan2512@gmail.com>
2019-03-15 11:43:22 +0900

    fix houmu-tab content

--- includes/custom-template/library/houmu-tab.php
+++ includes/custom-template/library/houmu-tab.php
@@ -1,14 +1,27 @@
 <div class="houmu">
         <div class="ml-15">
-            <h5>1．契約書雛型 <span style="color:red">※2018.12.18更新</span> </h5>
+            <h5>1．契約書に関するフロー（X-point 契約検討～捺印～保管)</h5>
             <div class="ml-55 mb-10">
-                <p>・雛型は常に改定されます。契約の都度、<span style="color:red"><b>ポータルサイトより最新のものを取得</b></span> のうえ、ご利用下さい。</p>
-                 <p><span style="color:red"><b>改定前の雛型を再利用することは禁止</b></span>します。</p>
-                <p>・雛型を変更、修正する際には、必ず事前に法務へご相談下さい。</p>
+                <p>契約書に関するフロー図　⇒　<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6Njc3ODY3YTJkODgzZGRiNw"><b>こちら</b></a></p>
+            </div>
+            <h5>2．契約書の検討依頼</h5>
+            <div class="ml-55 mb-10">
+                <p>・法務への契約検討依頼が必要なケースは、原則として次のとおりです。</p>
+                <div class="ml-55">
+                    <p>①新規ビジネス案件</p>
+                    <p>②社内ポータル掲載の契約書雛型以外の契約書を使用する場合</p>
+                    <p>③契約書雛型に相手方による修正があった場合</p>
+                </div>
+            </div>
+            <h5>3．契約書雛型 <span style="color:red">※2018.12.18更新</span> </h5>
+            <div class="ml-55 mb-10">
+                <p>・雛型は常に改定されます。<span style="text-decoration: underline;">契約の都度、</span><span style="color:red;text-decoration: underline;" ><b>ポータルサイトより最新のものを取得</b></span> のうえ、ご利用下さい。</p>
+                <p><span style="color:red"><b>改定前の雛型を再利用することは禁止</b></span>します。</p>
+                <p>・雛型を変更、修正する際には、必ず<span style="color:red"><b>事前に法務</span></b>へご相談下さい。</p>
                 <p class="mt-10 mb-10">契約書雛型の体系図　⇒ <a href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6ZmMwZTEwMTYzNzRlNTVh" target="_blank" ><b>こちら</b></a> </p>
                 <p>≪各部門が共通して使用する契約書雛型≫</p>
                 <div class="ml-55">
-                    <p>① <span style="color:#00aae1">秘密保持覚書</span> <span style="color:red">※2018.12.18更新 </span></p>
+                    <p>① <span style="color:#00aae1"> 秘密保持契約書</span> <span style="color:red">※2018.12.18更新 </span></p>
                     <p class="ml-15">
                         以下の2種ありますので、どちらを利用すべきか判断に迷う場合には、法務にご相談ください。 <br>
                         Ａ．<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6NDU2NjhkNWUyNjgxMzE1">秘密保持契約書_相互</a>  <br>
@@ -20,98 +33,53 @@
                         </div>
 
                     </p>
-                    <p>② <a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6NWE3MTYxMjY0ZGNiMjg3ZA"> 業務委託契約書 </a>　※当社が業務を委託する場合の雛型です。</p>
-                    <p>③ <a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6MzM4MGY2ZTFmNTQ0N2RlNQ"> 取引基本契約書　</a>※当社が発注する成果物の取引に関する雛型です。</p>
-                    <p>④ <a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6Njk2OGI2YTNkMGNjYjYyMA"> 個人情報保護に関する契約書　</a>※当社が個人情報の取扱いを委託する場合の雛型です。</p>
                 </div>
                 <p>≪出版社向け契約書雛型≫</p>
                 <p>●新規に基本契約を締結する出版社との間で締結する契約書雛型</p>
                 <div class="ml-55">
-                    <p>⑤<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6M2UxYTE1NjM5ZDA1YWU3OA">取次基本契約書-出版社様のコンテンツの配信許諾条件に関する契約書-</a> <span style="color:red">※2018.9.26更新</span></p>
+                    <p>②<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6M2UxYTE1NjM5ZDA1YWU3OA">取次基本契約書-出版社様のコンテンツの配信許諾条件に関する契約書-</a> <span style="color:red">※2018.9.26更新</span></p>
                     <div class="15">
                         <p> ※1. いわゆるホールセール（卸）形式の契約書</p>
                         <p> ※2. 本契約書には、基本的な契約条件に加え、<br>
-                            「⑦コンテンツの種別及び特約電子書店に関する覚書」及び「⑧配信データの制作に関する覚書」の内容が包含されています。</p>
+                        「④コンテンツの種別及び特約電子書店に関する覚書」及び「⑤読み放題サービスに関する覚書」の内容が包含されています。</p>
                         <p>   カバー部分の記入方法　⇒　<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6MzM2NWYyNzE1YWI0NjRjYQ"><b>こちら</b></a></p>
                         <p> ※3. Amazonは現在、電子書籍の同等性条項を行使しないこととしておりますので、<br>
                             　出版社との契約に際しては、出版社に対して同内容を記載した以下の通知を提示してください。</p>
                         <p>    Amazon「電子書籍の同等性条項に関するご通知」　⇒　　<a href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6NWRlYjRhZjMzMzExMjNmZA" target="_blank"><b>こちら</b></a>
                     </div>
-                    <p>⑥<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6MzI3ZGUyNTljZTFhN2U1OQ">配信業務委託基本契約書-出版社様のコンテンツの配信委託条件に関する契約書-</a> <span style="color:red">※2018.9.26更新</span></p>
+                    <p>③<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6MzI3ZGUyNTljZTFhN2U1OQ">配信業務委託基本契約書-出版社様のコンテンツの配信委託条件に関する契約書-</a> <span style="color:red">※2018.9.26更新</span></p>
                     <p class="ml-15">※いわゆるエージェンシー（委託）形式の契約書</p>
                 </div>
                 <p>●既に基本契約を締結している出版社との間で締結する覚書等 </p>
                 <div class="ml-55">
-                    <p><a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6MjM0NzNjMmY1MGYxN2JhNA">⑦コンテンツの種別及び特約電子書店に関する覚書</a></p>
+                    <p>④<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6MjM0NzNjMmY1MGYxN2JhNA">コンテンツの種別及び特約電子書店に関する覚書</a></p>
                     <p class="ml-15">※使用する場面：A.書籍、コミック、写真集、雑誌につき、追加で配信許諾を得た場合</p>
                     <p class="ml-55">B.Amazon等のサービスに特徴ある電子書店に配信するための特約</p>
                     <p class="ml-15">カバー部分の記入方法　⇒　<a href="https://sites.google.com/a/g.pubridge.jp/internal-portal/file-legal/%E3%80%90%E3%82%AB%E3%83%90%E3%83%BC%E8%A8%98%E5%85%A5%E6%96%B9%E6%B3%95%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB%E3%80%91%E3%82%B3%E3%83%B3%E3%83%86%E3%83%B3%E3%83%84%E3%81%AE%E7%A8%AE%E5%88%A5%E5%8F%8A%E3%81%B3%E7%89%B9%E7%B4%84%E9%9B%BB%E5%AD%90%E6%9B%B8%E5%BA%97%E3%81%AB%E9%96%A2%E3%81%99%E3%82%8B%E8%A6%9A%E6%9B%B8.pdf?attredirects=0&d=1"><b>こちら</b></a></p>
-                    <p>⑧<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6MWI5NjQ2MWQ1OTk4Njg3">配信データの制作に関する覚書</a>（基本契約を<span style="color:red">旧ビットウェイ</span>で締結した場合）</p>
-                    <p class="ml-15">※当社が配信データを制作する場合の覚書</p>
-                    <p>⑨<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6M2Y3NWEyNGJlOGQzYjM0NQ">配信データの制作に関する覚書</a>（基本契約を<span style="color:red">旧機構</span>で締結した場合） </p>
-                    <p class="ml-15">※当社が配信データを制作する場合の覚書</p>
-                    <p>⑩<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6NWE5YmI4MzVlMGYyOTg0OQ">配信許諾対価に関する覚書</a> </p>
                     <p class="ml-15">※配信許諾の対価を希望小売価格を基に算出するための覚書</p>
-                    <p>⑪<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6Mzg5MzU3NDExODNhZWYzMg">読み放題サービスに関する覚書</a> </p>
+                    <p>⑤<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6Mzg5MzU3NDExODNhZWYzMg">読み放題サービスに関する覚書</a> </p>
                     <p class="ml-15">※読み放題サービスに配信するための覚書</p>
-                    <p>⑫<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6NGQ5M2I2NWU3NGFlMzU5Mw">配信許諾電子書店及び料率等に関する確認書</a> </p>
+                    <p>⑥<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6NGQ5M2I2NWU3NGFlMzU5Mw">配信許諾電子書店及び料率等に関する確認書</a> </p>
                     <p class="ml-15">※角印運用かつ部門保管の確認書</p>
                 </div>
                 <p> ≪書店向け契約書雛型≫</p>
-                <p class="ml-55">⑬<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6MTA4OTg4ZTA2YjIwMmE5OQ">取次基本契約書-書店様へのコンテンツの配信許諾条件に関する契約書- </a></p>
+                <p class="ml-55">⑦<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6MTA4OTg4ZTA2YjIwMmE5OQ">取次基本契約書-書店様へのコンテンツの配信許諾条件に関する契約書- </a></p>
                 <div class="ml-55">
                     <p>※いわゆるホールセール（卸）形式の契約書</p>
                     <p class="ml-55">カバー部分の記入方法　⇒　<a target="_blank" href="https://sites.google.com/a/g.pubridge.jp/internal-portal/file-legal/%E3%80%90%E3%82%AB%E3%83%90%E3%83%BC%E8%A8%98%E5%85%A5%E6%96%B9%E6%B3%95%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB%E3%80%91%E5%8F%96%E6%AC%A1%E5%9F%BA%E6%9C%AC%E5%A5%91%E7%B4%84%E6%9B%B8_%E6%9B%B8%E5%BA%97%E6%A7%98.pdf?attredirects=0&d=1"><b>こちら</b></a></p>
                 </div>
-                <p class="ml-55">⑭<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6NjllMWQ3MjI3YTZmMjY3NQ">配信業務委託基本契約書-書店様へのコンテンツの配信委託条件に関する契約書-</a></p>
+                <p class="ml-55">⑧<a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6NjllMWQ3MjI3YTZmMjY3NQ">配信業務委託基本契約書-書店様へのコンテンツの配信委託条件に関する契約書-</a></p>
                 <div class="ml-55">
                     <p class="ml-15">※いわゆるホールセール（卸）形式の契約書</p>
                 </div>
-                <p class="ml-55">※電子書店との間で締結する基本契約の捺印申請時には、【契約稟議書兼捺印申請書の目的欄】に次の2点を記載してください。</p>
-                <p class="ml-55">A.帝国データバンクの評価点（信用程度を含む）<br>
-                    B.保証金の有無（有る場合には金額）</p>
             </div>
-            <h5>2. 契約書の検討依頼と捺印申請</h5>
+            <h5>4.印紙税について </h5>
             <div class="ml-55 mb-10">
-                <p>【契約検討依頼が必要なケース】</p>
-                <p class="ml-55">①新規ビジネス案件</p>
-                <p class="ml-55">②上記契約書雛型以外の契約書を使用する場合</p>
-                <p class="ml-55">③契約書雛型に相手方による修正があった場合</p>
-                <p>【契約検討依頼から捺印申請までの流れ】</p>
                 <div class="ml-55">
-                    <p>①契約検討依頼案件が発生</p>
-                    <p>②契約検討依頼に必要な書類を準備</p>
-                    <div class="ml-30">
-                        <p>・契約検討依頼書</p>
-                        <p>・検討対象契約書</p>
-                        <p>・関連契約書および資料等</p>
-                    </div>
-                    <p>③X-point「契約検討依頼書」による申請</p>
-                    <div class="ml-30">
-                        <p>※原契約は、捺印締結版のPDFファイルにて添付</p>
-                        <p>※その他検討に必要な資料を添付</p>
-                    </div>
-                    <p>④法務担当による検討</p>
-                    <div class="ml-30">
-                        <p>※契約検討は、通常２週間程度かかります。</p>
-                        <p class="ml-15">案件によっては更に時間を要する場合があるため、余裕をもって依頼するようにしてください。</p>
-                    </div>
-                    <p>⑤検討結果の返却</p>
-                    <p class="ml-30">※返却された契約検討依頼書は捺印申請時の添付書類です</p>
-                    <p>⑥部門でのリスクチェックおよび取引先との調整</p>
-                    <div class="ml-30">
-                        <p>※再度法務検討を要する場合は、X-point「契約再検討依頼書」により申請してください。</p>
-                        <p class="ml-15">「契約再検討依頼書」は、元のX-point「契約検討依頼書」の関連書類から申請します。</p>
-                    </div>
-                    <p>⑦取引先と契約内容が合意</p>
-                    <p>⑧捺印申請書の提出</p>
-                    <div class="ml-30">※【参考資料】<a target="_blank" href="https://docs.google.com/a/pubridge.jp/viewer?a=v&pid=sites&srcid=cHVicmlkZ2UuanB8aW50ZXJuYWwtcG9ydGFsfGd4OjIyY2E2NmU5YzUzNWNiOA">製本テープの使い方</a></div>
-                    <p>⑨印紙税額（抜粋）　⇒　<a target="_blank" href="https://docs.google.com/a/pubridge.jp/viewer?a=v&pid=sites&srcid=cHVicmlkZ2UuanB8aW50ZXJuYWwtcG9ydGFsfGd4OjQxOGZhZGQ0OTA3MzZlOTI"><b>こちら</b></a></p>
-                    <p>※ご不明な場合は、法務までお問い合わせ下さい。</p>
+                    <p>①印紙税額（抜粋）　⇒　<a target="_blank" href="https://docs.google.com/a/pubridge.jp/viewer?a=v&pid=sites&srcid=cHVicmlkZ2UuanB8aW50ZXJuYWwtcG9ydGFsfGd4OjQxOGZhZGQ0OTA3MzZlOTI"><b>こちら</b></a></p> 
                 </div>
-
             </div>
-            <h5>3. 契約書の閲覧およびPDF化について</h5>
+            <h5>5. 契約書の閲覧およびPDF化について</h5>
             <div class="ml-55 mb-10">
                 <div class="ml-55">
                 <p>①契約書の閲覧方法について</p>
@@ -125,20 +93,19 @@
                 </div>
             </div>
             <div class="ml-15 mb-10">
-                契約書PDFスキャン操作手順　⇒　<a target="_blank" href="https://docs.google.com/a/pubridge.jp/viewer?a=v&pid=sites&srcid=cHVicmlkZ2UuanB8aW50ZXJuYWwtcG9ydGFsfGd4OjJmN2FiNzI1NzhkMTUzMDc"><b>こちら</b></a>
+                <p>契約書PDFスキャン操作手順　⇒　<a target="_blank" href="https://docs.google.com/a/pubridge.jp/viewer?a=v&pid=sites&srcid=cHVicmlkZ2UuanB8aW50ZXJuYWwtcG9ydGFsfGd4OjJmN2FiNzI1NzhkMTUzMDc"><b>こちら</b></a></p>
+                <p>スキャナー機能の使い方　⇒　<a target="_blank" href="#"><b>こちら</b></a></p>
+            
             </div>
-            <h5>4. 保管用契約書の提出について</h5>
+            <h5>6. 保管用契約書の提出について</h5>
             <div class="ml-55 mb-10">
-                保管用の契約書原本は、所定の位置に設置された「保管用契約書BOX」に提出して下さい。
+                <p>保管用の契約書原本は、所定の位置に設置された「保管用契約書BOX」に提出して下さい。</p>
+                <p>5F：経営管理部前のキャビネット上にある「保管⽤契約書BOX」</p>
+                <p>8F：取次営業本部横のロッカーの上にある「保管⽤契約書BOX」</p>
             </div>
-            <h5>5. 反社会的勢力</h5>
+            <h5>7. 反社会的勢力</h5>
             <div class="ml-55 mb-10">
                 <p>当社が取引先と締結する契約書には、反社会的勢力の排除を規定する条文を必須とします。</p>
-                <p>契約書案に“反社会的勢力との関係性排除”の項目が規定されていない場合は、必ず追加してください。</p>
-                <p>締結済みの契約書の中に“反社会的勢力との関係性排除”の項目が規定されていない場合は、<a target="_blank" href="https://docs.google.com/a/pubridge.jp/viewer?a=v&pid=sites&srcid=cHVicmlkZ2UuanB8aW50ZXJuYWwtcG9ydGFsfGd4OjY0YzE0ODYxYjBmNmFjNjU">反社会的勢力排除に関する覚書</a>を締結してください。 </p>
-                <p><a target="_blank" href="https://docs.google.com/a/pubridge.jp/viewer?a=v&pid=sites&srcid=cHVicmlkZ2UuanB8aW50ZXJuYWwtcG9ydGFsfGd4OjY0YzE0ODYxYjBmNmFjNjU">・反社会的勢力排除に関する覚書</a></p>
-                <p><a target="_blank" href="https://sites.google.com/a/g.pubridge.jp/internal-portal/file-legal/%E4%BC%81%E6%A5%AD%E3%81%8C%E5%8F%8D%E7%A4%BE%E4%BC%9A%E7%9A%84%E5%8B%A2%E5%8A%9B%E3%81%AB%E3%82%88%E3%82%8B%E8%A2%AB%E5%AE%B3%E3%82%92%E9%98%B2%E6%AD%A2%E3%81%99%E3%82%8B%E3%81%9F%E3%82%81%E3%81%AE%E6%8C%87%E9%87%9D.pdf?attredirects=0&d=1">・企業が反社会的勢力による被害を防止するための指針</a></p>
-                <p><a target="_blank" href="https://sites.google.com/a/g.pubridge.jp/internal-portal/file-legal/%E4%BC%81%E6%A5%AD%E3%81%8C%E5%8F%8D%E7%A4%BE%E4%BC%9A%E7%9A%84%E5%8B%A2%E5%8A%9B%E3%81%AB%E3%82%88%E3%82%8B%E8%A2%AB%E5%AE%B3%E3%82%92%E9%98%B2%E6%AD%A2%E3%81%99%E3%82%8B%E3%81%9F%E3%82%81%E3%81%AE%E6%8C%87%E9%87%9D%E3%81%AB%E9%96%A2%E3%81%99%E3%82%8B%E8%A7%A3%E8%AA%AC.pdf?attredirects=0&d=1">・企業が反社会的勢力による被害を防止するための指針に関する解説</a></p>
             </div>
             ≪社外リンク≫
             <div class="ml-55">
@@ -148,7 +115,7 @@
             ----------------------------------------
             <div class="ml-55">
                 <p><b>契約書の情報を参照したいとき（契約書台帳関連）</b></p>
-                <p><a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6MzFlZmM4MWQwZjFkZjg1Yw">契約書台帳</a></p>
+                <p><a target="_blank" href="https://docs.google.com/a/g.pubridge.jp/viewer?a=v&pid=sites&srcid=Zy5wdWJyaWRnZS5qcHxpbnRlcm5hbC1wb3J0YWx8Z3g6MzFlZmM4MWQwZjFkZjg1Yw">契約台帳</a></p>
             </div>
         </div>
     </div>
