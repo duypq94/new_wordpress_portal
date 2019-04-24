81d8c5a5fa7b46e20987a2c56dd8679c8b35f217
duypq2394 <duypq.jp@gmail.com>
2019-04-24 14:09:53 +0900

    update view

--- includes/custom-template/library/employee_transition.php
+++ includes/custom-template/library/employee_transition.php
@@ -4,13 +4,30 @@
 			<p><b>TIMELINE: </b>&nbsp;&nbsp;
 				<select name="timeline" id="timeline" onchange=singleSelectChangeValue()>
 					<option value="2017">2017</option>                      
-					<option value="2018" selected="selected">2018</option>
-					<option value="2019">2019</option>
+					<option value="2018">2018</option>
+					<option value="2019" selected="selected">2019</option>
 				</select>
 			</p>
 		</div>
-		<div style="text-align: center;" >
-			<table id="table" class="table"></table>
+    <br>
+    <h5>MDHD</h5>
+		<div style="text-align: center; margin-top: 10px;" >
+			<table id="table_mdhd_nichi" class="table"></table>
+		</div>
+		<br>
+    <h5>MDHD</h5>
+		<div style="text-align: center; margin-top: 10px;" >
+			<table id="table_mdhd_matsujitsu" class="table"></table>
+		</div>
+		<br>
+    <h5>MD</h5>
+		<div style="text-align: center; margin-top: 10px;" >
+			<table id="table_md_nichi" class="table"></table>
+		</div>
+		<br>
+    <h5>MD</h5>
+		<div style="text-align: center; margin-top: 10px;" >
+			<table id="table_md_matsujitsu" class="table"></table>
 		</div>
 	</div>
 </div>
@@ -25,7 +42,7 @@ async function checkLastMonth() {
 	var result = [];
 	await axios.get(`https://sheets.googleapis.com/v4/spreadsheets/1w1jC8OG5z64eu8B0Ur_4NGip_0Im6TaKEBSOtAoNkT4/values/${lastMonth}?key=AIzaSyACRi5yaAcwIOF9QY3Fo1ful5AAyC38NEI`)
 		.then(function (response) {
-			for(var i = 0; i<4; i++) {
+			for(var i = 0; i<11; i++) {
 				result[i] = response.data.values[i][12];
 			}
 		})
@@ -33,30 +50,38 @@ async function checkLastMonth() {
 	return result;
 }
 
-async function renderTable(){
-	var newArr = new Array([4], [13]); 
-	var changeValue = new Array([4], [13]); 
-	var renderValue;
-	var selObj = document.getElementById("timeline");
-	var selValue = selObj.options[selObj.selectedIndex].text;
-	var lastMonthValue = await checkLastMonth();
-	await axios.get(`https://sheets.googleapis.com/v4/spreadsheets/1w1jC8OG5z64eu8B0Ur_4NGip_0Im6TaKEBSOtAoNkT4/values/${selValue}?key=AIzaSyACRi5yaAcwIOF9QY3Fo1ful5AAyC38NEI`)
-  	.then(function (response) 
-		{
-	  	for(var i = 0; i<4; i++){
-		  		if(!newArr[i]) {
+function writeTable(response, location, title, lastMonthValue){
+  var newArr = new Array([10], [13]); 
+	var changeValue = new Array([10], [13]); 
+  var taps = new Array(11);
+  var month = 0;
+  taps[0] = `<th>${response.data.values[location][0]}</th>`;
+  taps[1] = '<td><b>役員</b></td>	';
+  taps[2] = '<td><b>社員<br>（執行役員含）</b></td>	';
+  taps[3] = '<td><b>社員内<br>（出向出し）</b></td>	';
+  taps[4] = '<td><b>社員<br>（出向受け）</b></td>	';
+  taps[5] = '<td><b>嘱託</b></td>';
+  taps[6] = '<td><b>契約社員</b></td>';
+  taps[7] = '<td><b>アルバイト</b></td>';
+  taps[8] = '<td><b>パート</b></td>';
+  taps[9] = '<td><b>派遣</b></td>';
+  taps[10] = '<td style="background-color: #fff2cc;"><b>合計</b></td>';
+	for(var i = 0; i<11; i++){
+    tabtitle = title + i.toString()
+		if(!newArr[i]) {
 					newArr[i]=[];
 		  		}
 		  		if(!changeValue[i]) {
 					changeValue[i]=[];
 		  		}
-				for(var j = 0; j<13; j++){
-					if(response.data.values[i][j]) {
-						newArr[i][j]=response.data.values[i][j];
-						if( j === 1 && lastMonthValue.length === 4){
-							count= response.data.values[i][j]-lastMonthValue[i];
+        var row = i + location; 
+				for(var j = 1; j<13; j++){
+					if(response.data.values[row][j]) {
+						newArr[i][j]=response.data.values[row][j];
+						if( j === 1 && lastMonthValue.length === 10){
+							count= response.data.values[row][j]-lastMonthValue[i];
 						} else {
-							count= response.data.values[i][j]-response.data.values[i][j-1];
+							count= response.data.values[row][j]-response.data.values[row][j-1];
 						}
 							if(Number.isNaN(count) || count === 0) {
 								changeValue[i][j]="";
@@ -72,70 +97,47 @@ async function renderTable(){
 						newArr[i][j] = "ー";
 						changeValue[i][j]="";
 					}
+          switch(i) {
+            case 0:
+             taps[0] +=`<th>${j}月</th>`;
+             break;
+            case 10:
+             taps[10] +=`<td style="background-color: #fff2cc;">${newArr[i][j]} <br>${changeValue[i][j]}</td>`;
+             break;
+            default:
+             taps[i] +=`<td>${newArr[i][j]} <br>${changeValue[i][j]}</td>`;
+          }
 				} 
+        document.getElementById(tabtitle).innerHTML= taps[i];
 	  	}
-		renderValue = `
-		<tr>
-			<th>&nbsp;</th>
-				<th>1月</th>
-				<th>2月</th>
-			<th>3月</th>
-			<th>4月</th>
-			<th>5月</th>
-			<th>6月</th>
-			<th>7月</th>
-			<th>8月</th>
-			<th>9月</th>
-			<th>10月</th>
-			<th>11月</th>
-			<th>12月</th>
-			</tr>
-			<tr>
-				<td>正社員</td>	
-			<td>${newArr[1][1]} <br>${changeValue[1][1]}</td>
-			<td>${newArr[1][2]} <br>${changeValue[1][2]}</td>
-			<td>${newArr[1][3]} <br>${changeValue[1][3]}</td>
-			<td>${newArr[1][4]} <br>${changeValue[1][4]}</td>
-			<td>${newArr[1][5]} <br>${changeValue[1][5]}</td>
-			<td>${newArr[1][6]} <br>${changeValue[1][6]}</td>
-			<td>${newArr[1][7]} <br>${changeValue[1][7]}</td>
-			<td>${newArr[1][8]} <br>${changeValue[1][8]}</td>
-			<td>${newArr[1][9]} <br>${changeValue[1][9]}</td>
-			<td>${newArr[1][10]} <br>${changeValue[1][10]}</td>
-			<td>${newArr[1][11]} <br>${changeValue[1][11]}</td>
-			<td>${newArr[1][12]} <br>${changeValue[1][12]}</td>
-			</tr>
-			<tr>
-			<td>派遣</td>
-			<td>${newArr[2][1]} <br>${changeValue[2][1]}</td>
-			<td>${newArr[2][2]} <br>${changeValue[2][2]}</td>
-			<td>${newArr[2][3]} <br>${changeValue[2][3]}</td>
-			<td>${newArr[2][4]} <br>${changeValue[2][4]}</td>
-			<td>${newArr[2][5]} <br>${changeValue[2][5]}</td>
-			<td>${newArr[2][6]} <br>${changeValue[2][6]}</td>
-			<td>${newArr[2][7]} <br>${changeValue[2][7]}</td>
-			<td>${newArr[2][8]} <br>${changeValue[2][8]}</td>
-			<td>${newArr[2][9]} <br>${changeValue[2][9]}</td>
-			<td>${newArr[2][10]} <br>${changeValue[2][10]}</td>
-			<td>${newArr[2][11]} <br>${changeValue[2][11]}</td>
-			<td>${newArr[2][12]} <br>${changeValue[2][12]}</td>
-			</tr>
-			<tr>
-			<td>業務委託</td>
-			<td>${newArr[3][1]} <br>${changeValue[3][1]}</td>
-			<td>${newArr[3][2]} <br>${changeValue[3][2]}</td>
-			<td>${newArr[3][3]} <br>${changeValue[3][3]}</td>
-			<td>${newArr[3][4]} <br>${changeValue[3][4]}</td>
-			<td>${newArr[3][5]} <br>${changeValue[3][5]}</td>
-			<td>${newArr[3][6]} <br>${changeValue[3][6]}</td>
-			<td>${newArr[3][7]} <br>${changeValue[3][7]}</td>
-			<td>${newArr[3][8]} <br>${changeValue[3][8]}</td>
-			<td>${newArr[3][9]} <br>${changeValue[3][9]}</td>
-			<td>${newArr[3][10]} <br>${changeValue[3][10]}</td>
-			<td>${newArr[3][11]} <br>${changeValue[3][11]}</td>
-			<td>${newArr[3][12]} <br>${changeValue[3][12]}</td>
-			</tr>`
-		document.getElementById('table').innerHTML= renderValue;
+}
+
+async function renderTable(){
+	var renderValue;
+	var selObj = document.getElementById("timeline");
+	var selValue = selObj.options[selObj.selectedIndex].text;
+	var lastMonthValue = await checkLastMonth();
+	var tabelMDHD1 = "";
+	var tabelMDHD2 = "";
+	var tabelMD1 = ""; 
+	var tabelMD2 = "";
+	await axios.get(`https://sheets.googleapis.com/v4/spreadsheets/1w1jC8OG5z64eu8B0Ur_4NGip_0Im6TaKEBSOtAoNkT4/values/${selValue}?key=AIzaSyACRi5yaAcwIOF9QY3Fo1ful5AAyC38NEI`)
+  	.then(function (response) 
+		{
+			for(var i = 0; i < 11; i++){
+				tabelMDHD1 += `<tr id="mdhd_nichi_${i}"></tr>`;
+				tabelMDHD2 += `<tr id="mdhd_matsujitsu_${i}"></tr>`;
+				tabelMD1 += `<tr id="md_nichi_${i}"></tr>`;
+				tabelMD2 += `<tr id="md_matsujitsu_${i}"></tr>`;
+			}
+		document.getElementById('table_mdhd_nichi').innerHTML= tabelMDHD1;
+		document.getElementById('table_mdhd_matsujitsu').innerHTML= tabelMDHD2;
+		document.getElementById('table_md_nichi').innerHTML= tabelMD1;
+		document.getElementById('table_md_matsujitsu').innerHTML= tabelMD2;
+    writeTable(response,1,"mdhd_nichi_","");
+		writeTable(response,14,"mdhd_matsujitsu_","");
+		writeTable(response,27,"md_nichi_","");
+		writeTable(response,40,"md_matsujitsu_","");
 			}).catch(function (error) { console.log(error);});
 }
 
